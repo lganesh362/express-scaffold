2eeff84df62f1fd7c8956f74494927c74f04d869
lganesh362 <127839676+lganesh362@users.noreply.github.com>
2023-04-13 18:54:19 +0530

    Update checkerRoutes.js

--- routes/checker/checkerRoutes.js
+++ routes/checker/checkerRoutes.js
@@ -10,7 +10,7 @@ const logger = require('../../logger/logger')
  */
 const sayHello = (req, res) => {
     logger.info(`Checker route! Saying hello from ${req.originalUrl}`)
-    res.status(200).json({ "message": "Hello Express! I am ready for work13042023" })
+    res.status(200).json({ "message": "Hello Express! I am ready for work" })
 }
 
 module.exports = {
